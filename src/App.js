5c96e576196d7a44dce14c02f5d6cfa3cebb5a87
alexviarhey <lexa.vergey@gmail.com>
2019-12-02 11:56:59 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Line} from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const dataFromServer = [
+        {studentCount: 200, year: 2018 , month: 1},
+        {studentCount: 100, year: 2019, month: 2},
+        {studentCount: 300, year: 2020, month: 3},
+    ];
+
+    const dataFromServer1 = [
+        {studentCount: 500, year: 2018 , month: 1},
+        {studentCount: 400, year: 2019, month: 2},
+        {studentCount: 200, year: 2020, month: 3},
+    ];
+
+    const data = {
+        datasets: [
+            {
+                data: dataFromServer.map(point => ({x: new Date(point.year, point.month), y: point.studentCount})),
+                lineTension: 0
+            },
+
+            {
+                data: dataFromServer1.map(point => ({x: new Date(point.year, point.month), y: point.studentCount})),
+                lineTension: 0
+            }
+        ]
+    };
+
+    const options = {
+        scales:
+            {
+                xAxes: [
+                    {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
+                    }
+                ],
+
+                yAxes: [
+                    {
+                        ticks: {
+                            min: 50
+                        }
+                    }
+                ]
+            }
+    };
+
+
+    return (
+        <div className='App'>
+
+                <Line data={data}
+                      options={options}
+                />
+
+
+        </div>
+    )
+};
 
 export default App;
